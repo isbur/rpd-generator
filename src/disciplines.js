382a9631ebaa813a2c5c69250dd3dacf96ab97d7
Igor Burenkov <isburenkov@gmail.com>
2019-11-14 23:12:24 +0000

    Separated main.js from disciplines.js, started to create full automatization script

--- src/disciplines.js
+++ src/disciplines.js
@@ -1,29 +1,4 @@
 /**
- * @file Ни много ни мало основной файл
- */
-
-/**
- * Добавляем к результатирующий таблице пункт меню "Скрипты"
- * с подменю "Сделать выгрузку", "Создать контентные шаблоны" и "Создать РПД"
- * при нажатии на которые запускаются соответствующие функции
- * "getDisciplines", "createTemplates", "createRPD"
- * @see getDisciplines
- * @see disciplines.js (this file)
- * @see createTemplates
- * @see templates.js
- * @see createRPD
- * @see RPD.js
- */
-function onOpen() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  ss.addMenu('Скрипты', [
-    {name: 'Сделать выгрузку', functionName: 'getDisciplines'},
-    {name: 'Создать контентные шаблоны', functionName: 'createTemplates'},
-    {name: 'Создать РПД', functionName: 'createRPD'}
-  ]);
-}
-
-/**
  * Cоздание списка дисциплин из УП
  * Хотя непонятно, чего я ожидал, производя поиск здесь - ошибка-то DriveApp вываливается при запуске генерации контентных файлов... Facepalm.
  *
